36d5139566a2480d507ad0520022d312840eaaf4
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-02-13 16:46:03 +0100

    Update .gitignore and buildjsonld.py

--- src/scripts/types/Award/buildjsonld.py
+++ src/scripts/types/Award/buildjsonld.py
@@ -12,14 +12,13 @@ df = df.drop(df.columns[[0, 1]], axis=1)
 print(df)
 
 # Convert the DataFrame to JSON-LD
-json_data = df.to_json(orient="records", lines=True)
+json_data = df.to_json(orient="index", lines=True)
 
 print(json_data)
 
-#json_ld = json.loads(json_data)
+json_data = json.loads(json_data)
+
 
 # Save the JSON-LD to a file
-with open("data.jsonld", "w") as file:    
-    json.dump(json_data, file)
-    file_path = "/d:/Projekte/opendatatourism.ch/opendatatourism.ch/data/data.csv"
-    df.to_csv(file_path, index=False)
+with open("output/data.jsonld", "w", encoding='utf-8') as file:    
+    json.dump(json_data, file, ensure_ascii=False, indent=4)
